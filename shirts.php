4c061d19dd14a2b34eafb4ffa821f4d5e4e1887c
Maria Fischinger <mmfischinger@gmail.com>
2013-11-09 22:01:38 -0600

    first commit

---
+++ shirts.php
@@ -0,0 +1,11 @@
+<?php
+$pageTitle = "Mikes full catalog of shirts";
+$section = "shirts";
+include('inc/header.php'); ?>
+<title><?php echo $pageTitle ?></title>
+  <div class="section page">
+    <h1>Mike&rsquo;s Full Catalog of Shirts</h1>
+
+
+
+<?php include('inc/footer.php'); ?>
