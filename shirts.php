57588991d39016819a622c02c13e19db990024f3
Maria Fischinger <mmfischinger@gmail.com>
2013-11-09 22:51:20 -0600

    work with arrays

--- shirts.php
+++ shirts.php
@@ -1,11 +1,30 @@
 <?php
-$pageTitle = "Mikes full catalog of shirts";
+
+$products = array();
+$products[101] = "Logo Shirt, Red";
+$products[102] = "Mike the Frog Shirt, Black";
+$products[103] = "Mike the Frog Shirt, Blue";
+$products[104] = "Logo Shirt, Green";
+
+?><?php
+$pageTitle = "Mikes Full Catalog of Shirts";
 $section = "shirts";
 include('inc/header.php'); ?>
-<title><?php echo $pageTitle ?></title>
-  <div class="section page">
-    <h1>Mike&rsquo;s Full Catalog of Shirts</h1>
 
+  <div class="section page">
+
+    <div class="wrapper">
+
+      <h1>Mike&rsquo;s Full Catalog of Shirts</h1>
+
+        <ul>
+          <?php foreach($products as $product) { ?>
+            <li><?php echo $product; ?></li>
+          <?php } ?>
+        </ul>
+
+    </div>
+  </div>
 
 
 <?php include('inc/footer.php'); ?>
